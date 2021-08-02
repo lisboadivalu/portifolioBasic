782cd00c6edf5838e006c9656f944a872fb1c007
Lisboa <lisboadivalu@gmail.com>
2021-08-02 15:55:38 -0300

    melhoria na pagina

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 function menuFunction(x)
-        {
-            x.classList.toggle("change");
-        }
+    {
+        x.classList.toggle("change");
+    }
 
 $(document).ready(function(){
     $('.burguer-container').on('click', function(){
+        $('.main-navigation').toggle('slow')
+    })
+})
+
+$(document).ready(function(){
+    $('.close').on('click', function(){
         $('.main-navigation').toggle('slow')
     })
 })
